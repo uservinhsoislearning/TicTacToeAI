cd3ad8adaab395d9a35ecd795908eff68bef7047
VinhTran <vinhthanhtran03102004@gmail.com>
2024-10-14 23:02:08 +0700

    delete the old solver

--- Python_tictactoe/tictactoe.py
+++ Python_tictactoe/tictactoe.py
@@ -68,35 +68,6 @@ def check_win(player, check_board=board):
         return True
 
     return False
-
-# def minimax(minimax_board, depth, is_max):
-#     if check_win(2, minimax_board):
-#         return float('inf')
-#     elif check_win(1, minimax_board):
-#         return float('-inf')
-#     elif is_board_full(minimax_board):
-#         return 0
-    
-#     if is_max: # If best what should com do ?
-#         best_score = -1000
-#         for row in range(CONST.BOARD_ROWS):
-#             for col in range(CONST.BOARD_COLS):
-#                 if minimax_board[row][col] == 0:
-#                     minimax_board[row][col] = 2
-#                     score = minimax(minimax_board, depth+1, False)
-#                     minimax_board[row][col] = 0
-#                     best_score = max(score, best_score)
-#         return best_score
-#     else:
-#         best_score = 1000
-#         for row in range(CONST.BOARD_ROWS):
-#             for col in range(CONST.BOARD_COLS):
-#                 if minimax_board[row][col] == 0:
-#                     minimax_board[row][col] = 1
-#                     score = minimax(minimax_board, depth+1, True)
-#                     minimax_board[row][col] = 0
-#                     best_score = min(score, best_score)
-#         return best_score
     
 def best_move():
     best_score = -1000
