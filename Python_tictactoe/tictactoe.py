f278d58dd4abe6c1893e7c16fa6b9c375b373254
VinhTran <vinhthanhtran03102004@gmail.com>
2024-10-14 22:28:55 +0700

    fixing some bugs

--- Python_tictactoe/tictactoe.py
+++ Python_tictactoe/tictactoe.py
@@ -4,45 +4,38 @@ import constants as CONST
 
 pygame.init()
 
-# Colors
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-BLUE = (0, 0, 225)
-RED = (255, 0, 0)
-GREEN = (0, 225, 0)
-
-# CONST
-WIDTH = 300
-HEIGHT = 300
-LINE_WIDTH = 5
-BOARD_ROWS = 3
-BOARD_COLS = 3
-CELL_SIZE = WIDTH // BOARD_COLS
-O_RADIUS = CELL_SIZE // 3
-O_WIDTH = 15
-X_WIDTH = 25
-
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen = pygame.display.set_mode((CONST.WIDTH, CONST.HEIGHT))
 pygame.display.set_caption('Tic Tac Toe')
-screen.fill(WHITE)
+screen.fill(CONST.WHITE)
 
-board = np.zeros((BOARD_ROWS,BOARD_COLS))
+board = np.zeros((CONST.BOARD_ROWS,CONST.BOARD_COLS))
 
-def draw_lines(color=BLACK):
-    for i in range(1, BOARD_ROWS):
-        pygame.draw.line(screen, color, (0,CELL_SIZE*i), (WIDTH, CELL_SIZE*i),  LINE_WIDTH)
-        pygame.draw.line(screen, color, (CELL_SIZE*i, 0), (CELL_SIZE*i, HEIGHT),  LINE_WIDTH)
+def draw_lines(color=CONST.BLACK):
+    for i in range(1, CONST.BOARD_ROWS):
+        pygame.draw.line(screen, color, (0,CONST.CELL_SIZE*i), (WIDTH, CONST.CELL_SIZE*i),  CONST.LINE_WIDTH)
+        pygame.draw.line(screen, color, (CONST.CELL_SIZE*i, 0), (CONST.CELL_SIZE*i, CONST.HEIGHT),  CONST.LINE_WIDTH)
 
-def draw_figures(color=BLACK):
-    for row in range(BOARD_ROWS):
-        for col in range(BOARD_COLS):
+def draw_figures(color=CONST.BLACK):
+    for row in range(CONST.BOARD_ROWS):
+        for col in range(CONST.BOARD_COLS):
             if board[row][col] == 1:
-                pygame.draw.circle(screen, color, (int(col*CELL_SIZE + CELL_SIZE // 2), int(row*CELL_SIZE + CELL_SIZE // 2)), O_RADIUS, O_WIDTH)
+                pygame.draw.circle(screen, 
+                                   color, 
+                                   (int(col*CONST.CELL_SIZE + CONST.CELL_SIZE // 2), int(row*CONST.CELL_SIZE + CONST.CELL_SIZE // 2)), 
+                                   CONST.O_RADIUS, 
+                                   CONST.O_WIDTH)
             elif board[row][col] == 2:
-                pygame.draw.line(screen, color, (col*CELL_SIZE + CELL_SIZE // 4, row*CELL_SIZE + CELL_SIZE // 4)
-                                 , (col*CELL_SIZE + 3*CELL_SIZE // 4, row*CELL_SIZE + 3*CELL_SIZE // 4), X_WIDTH)
-                pygame.draw.line(screen, color, (col*CELL_SIZE + CELL_SIZE // 4, row*CELL_SIZE + 3*CELL_SIZE // 4)
-                                 , (col*CELL_SIZE + 3*CELL_SIZE // 4, row*CELL_SIZE + CELL_SIZE // 4), X_WIDTH)
+                pygame.draw.line(screen, 
+                                 color, 
+                                 (col*CONST.CELL_SIZE + CONST.CELL_SIZE // 4, row*CONST.CELL_SIZE + CONST.CELL_SIZE // 4), 
+                                 (col*CONST.CELL_SIZE + 3*CONST.CELL_SIZE // 4, row*CONST.CELL_SIZE + 3*CONST.CELL_SIZE // 4), 
+                                 CONST.X_WIDTH)
+                
+                pygame.draw.line(screen, 
+                                 color, 
+                                 (col*CONST.CELL_SIZE + CONST.CELL_SIZE // 4, row*CONST.CELL_SIZE + 3*CONST.CELL_SIZE // 4), 
+                                 (col*CONST.CELL_SIZE + 3*CONST.CELL_SIZE // 4, row*CONST.CELL_SIZE + CONST.CELL_SIZE // 4), 
+                                 CONST.X_WIDTH)
 
 def mark_square(row, col, player):
     board[row][col] = player
@@ -51,19 +44,19 @@ def available_square(row, col):
     return board[row][col] == 0
 
 def is_board_full(check_board=board):
-    for row in range(BOARD_ROWS):
-        for col in range(BOARD_COLS):
+    for row in range(CONST.BOARD_ROWS):
+        for col in range(CONST.BOARD_COLS):
             if check_board[row][col] == 0:
                 return False
             
     return True
 
 def check_win(player, check_board=board):
-    for col in range(BOARD_COLS):
+    for col in range(CONST.BOARD_COLS):
         if check_board[0][col] == player and check_board[1][col] == player and check_board[2][col] == player:
             return True
         
-    for row in range(BOARD_ROWS):
+    for row in range(CONST.BOARD_ROWS):
         if check_board[row][0] == player and check_board[row][1] == player and check_board[row][2] == player:
             return True
         
@@ -85,8 +78,8 @@ def minimax(minimax_board, depth, is_max):
     
     if is_max: # If best what should com do ?
         best_score = -1000
-        for row in range(BOARD_ROWS):
-            for col in range(BOARD_COLS):
+        for row in range(CONST.BOARD_ROWS):
+            for col in range(CONST.BOARD_COLS):
                 if minimax_board[row][col] == 0:
                     minimax_board[row][col] = 2
                     score = minimax(minimax_board, depth+1, False)
@@ -95,8 +88,8 @@ def minimax(minimax_board, depth, is_max):
         return best_score
     else:
         best_score = 1000
-        for row in range(BOARD_ROWS):
-            for col in range(BOARD_COLS):
+        for row in range(CONST.BOARD_ROWS):
+            for col in range(CONST.BOARD_COLS):
                 if minimax_board[row][col] == 0:
                     minimax_board[row][col] = 1
                     score = minimax(minimax_board, depth+1, True)
@@ -107,8 +100,8 @@ def minimax(minimax_board, depth, is_max):
 def best_move():
     best_score = -1000
     move = (-1,-1)
-    for row in range(BOARD_ROWS):
-        for col in range(BOARD_COLS):
+    for row in range(CONST.BOARD_ROWS):
+        for col in range(CONST.BOARD_COLS):
             if board[row][col] == 0:
                 board[row][col] = 2
                 score = minimax(board, 0, False)
@@ -123,8 +116,8 @@ def best_move():
     return False
 
 def restart_game():
-    screen.fill(WHITE)
+    screen.fill(CONST.WHITE)
     draw_lines()
-    for row in range(BOARD_ROWS):
-        for col in range(BOARD_COLS): 
+    for row in range(CONST.BOARD_ROWS):
+        for col in range(CONST.BOARD_COLS): 
             board[row][col] = 0
